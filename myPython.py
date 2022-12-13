60d91f7456375cad55a5d71dc34fdc5eb12a3cd7
Test-K12 <111693875+Test-K12@users.noreply.github.com>
2022-12-13 17:49:56 +0300

    Add files via upload

---
+++ myPython.py
@@ -0,0 +1,87 @@
+# # # # # def test(a, b):
+# # # # #     return a+b==3
+# # # # # t = test(5, 3)
+# # # # # print(t)
+
+# # # # # def getAvg(a, b):
+# # # #   #  sum=a+b
+# # # #    # print(sum/2)
+
+# # # # #getAvg(13, 34)    
+
+# # # # # def sum(a, b):
+# # # # #     return a+b
+
+# # # # # def sub(a, b):
+# # # # #     return a-b
+
+# # # # # def mul(a, b):
+# # # # #     return a*b
+
+# # # # # def div(a, b):
+# # # # #     return a/b
+
+
+
+
+
+
+# # # # def functionA():
+# # # #     print("A1")
+# # # #     functionB()
+# # # #     print("A2")
+# # # # def functionB():
+# # # #     print("B1")
+# # # #     functionC()
+# # # #     print("B2")
+# # # # def functionC():
+# # # #     print("C1")
+
+
+# # # # functionA()
+
+
+
+# # # age = 18
+
+# # # if age<=18:
+# # #     print("Пейте сок")
+# # # else:
+# # #     print("не сок")
+
+
+# for x in range(0, 10):
+#     print(x)
+
+# nums=[2,4,5,6,2,1,6,6,7]
+# l = len(nums)
+# for x in range(l):
+#     print(nums[x])
+
+
+# # names=['boris', 'alex', 'slava', 'mark']
+# # l=len(names)
+# # for x in range(l):
+# #     print(names[x])
+
+# # nums=[20,30,40,70,80,100, 99]
+# # l = len(nums)
+# # for x in range(l-1, -1, -1):
+# #      if nums[x] < 50:
+# #          print(nums[x])
+
+
+
+
+
+
+
+# # from classPython import Student
+
+# # s1 = Student("f1", "f2", "f", 5)
+# # # s2 = Student('a', 'b', 'c', 12)
+# # # print(s1.fName)
+# # # print(s2.fName)
+
+# # s1.name()
+# # s1.GetAge()
