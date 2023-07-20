b00306065eab63b92ee45ffec9569c01476948b1
Alex Kan <q7978057@gmail.com>
2023-07-20 11:46:23 +0300

    add_file_Stepik

---
+++ STEPIK.py
@@ -0,0 +1,35 @@
+# name = input()
+# name1 = input()
+# name2 = input()
+# print(name + '\n' + name1 + "\n" + name2)
+
+# print('a', 'b', 'c', sep='*')
+# print('d', 'e', 'f', sep='**')
+
+
+# print('a', 'b', 'c', end='@')
+# print('d', 'e', 'f', end='@@')
+
+
+# print('a', 'b', 'c', sep='*', end=' - finish')
+# print('d', 'e', 'f', sep='**', end='^__^')
+
+# print('Python', end='\n\n\n')
+
+
+# n = input()
+# n1 = input()
+# n2 = input()
+# n3 = input()
+# print(n1, n2, n3, sep = n)
+
+# n,n1,n2 = int(input()), int(input()), int(input())
+# print(n + n1 + n2)
+
+# a = int(input())
+# b = int(input())
+# print(a, "+", b, "=", a + b)
+# print(a, "-", b, "=", a - b)
+# print(a, "*", b, "=", a * b)
+
+
